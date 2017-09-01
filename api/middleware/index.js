76e0204524fdbd27c7260af50ef83cbacfbb5b87
Eric Mai <enmai1988@gmail.com>
2017-09-01 14:17:44 -0700

    added passport with facebook

--- api/middleware/index.js
+++ api/middleware/index.js
@@ -1,2 +1,6 @@
 module.exports.passport = require('./passport');
+<<<<<<< HEAD
 module.exports.auth = require('./auth');
+=======
+module.exports.auth = require('./auth');
+>>>>>>> added passport with facebook
