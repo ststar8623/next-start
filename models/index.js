ffb9e4fb1f67997ab84369f94b845d6b46e68c48
ststar8623 <ststar8623@gmail.com>
2017-09-01 14:17:42 -0700

    migrate db

--- models/index.js
+++ models/index.js
@@ -3,18 +3,25 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const _ = require('lodash');
 const config = require('config')['sequelize'];
+<<<<<<< 398466592b2660edf4eaa828b2c818441b04712d
 <<<<<<< ff3d972a585961848d9cedb97c787b9313d98eac
 <<<<<<< 3ad2161f45f883c4ac8673e2ab83252437115d45
+=======
+<<<<<<< befd75a767e5277821d5046005fd7ca7095e4620
+>>>>>>> migrate db
 
 let db = {};
 =======
 var db        = {};
 
 >>>>>>> migrate db
+<<<<<<< 398466592b2660edf4eaa828b2c818441b04712d
 =======
 
 let db = {};
 >>>>>>> database setup
+=======
+>>>>>>> migrate db
 // TODO - per-environment config
 const sequelize = new Sequelize(config.database, config.user, config.password, {
   host: config.host,
@@ -24,6 +31,7 @@ const sequelize = new Sequelize(config.database, config.user, config.password, {
 fs
   .readdirSync(__dirname)
   .filter(function(file) {
+<<<<<<< 398466592b2660edf4eaa828b2c818441b04712d
 <<<<<<< ff3d972a585961848d9cedb97c787b9313d98eac
 <<<<<<< 3ad2161f45f883c4ac8673e2ab83252437115d45
     console.log('file', file);
@@ -35,6 +43,14 @@ fs
     console.log('file', file);
     return file.indexOf('.') !== 0 && file !== 'index.js';
 >>>>>>> database setup
+=======
+<<<<<<< befd75a767e5277821d5046005fd7ca7095e4620
+    console.log('file', file);
+    return file.indexOf('.') !== 0 && file !== 'index.js';
+=======
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+>>>>>>> migrate db
+>>>>>>> migrate db
   })
   .forEach(function(file) {
     var model = sequelize['import'](path.join(__dirname, file));
@@ -47,6 +63,7 @@ Object.keys(db).forEach(function(modelName) {
   }
 });
 
+<<<<<<< 398466592b2660edf4eaa828b2c818441b04712d
 <<<<<<< 4892548d25fa3ee9d1ea8d63fc3d83cec78bff9b
 <<<<<<< ff3d972a585961848d9cedb97c787b9313d98eac
 <<<<<<< 3ad2161f45f883c4ac8673e2ab83252437115d45
@@ -69,6 +86,9 @@ db.image.belongsToMany(db.topping, { through: 'topping_images' });
 =======
 sequelize.sync();
 =======
+=======
+<<<<<<< befd75a767e5277821d5046005fd7ca7095e4620
+>>>>>>> migrate db
 sequelize.sync({ force: true });
 >>>>>>> Feat/database
 
@@ -89,5 +109,13 @@ db.image.belongsToMany(db.menu, { through: 'menu_images' });
 db.topping.belongsToMany(db.image, { through: 'topping_images' });
 db.image.belongsToMany(db.topping, { through: 'topping_images' });
 
+<<<<<<< 398466592b2660edf4eaa828b2c818441b04712d
 >>>>>>> Feat/database
+=======
+=======
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+>>>>>>> migrate db
+>>>>>>> migrate db
 module.exports = db;
