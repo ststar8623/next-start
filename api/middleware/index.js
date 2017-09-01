511923b6cc70f308795c90f89df14cc503f473c3
Eric Mai <enmai1988@gmail.com>
2017-08-31 19:09:07 -0700

    added LRUCache and re-configured server

--- api/middleware/index.js
+++ api/middleware/index.js
@@ -1,6 +1,2 @@
 module.exports.passport = require('./passport');
-<<<<<<< HEAD
 module.exports.auth = require('./auth');
-=======
-module.exports.auth = require('./auth');
->>>>>>> added passport with facebook
