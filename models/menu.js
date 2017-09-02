7057386aa6cc38f0115f9df607b4adcf05d73fbd
ststar8623 <ststar8623@gmail.com>
2017-09-01 17:16:10 -0700

    Finish postgres setup

--- models/menu.js
+++ models/menu.js
@@ -1,20 +1,12 @@
-<<<<<<< ff3d972a585961848d9cedb97c787b9313d98eac
 module.exports = (db, DataTypes) => {
   const Menu = db.define('menu', {
     id: {
       type: DataTypes.INTEGER,
-=======
-module.exports = (db, DataType) => {
-  const Menu = db.define('menu', {
-    id: {
-      type: DataType.INTEGER,
->>>>>>> database setup
       autoIncrement: true,
       allowNull: false,
       primaryKey: true
     },
     name: {
-<<<<<<< ff3d972a585961848d9cedb97c787b9313d98eac
       type: DataTypes.STRING,
       allowNull: false
     },
@@ -23,16 +15,6 @@ module.exports = (db, DataType) => {
       allowNUll: false
     },
     description: DataTypes.TEXT
-=======
-      type: DataType.STRING,
-      allowNull: false
-    },
-    price: {
-      type: DataType.INTEGER,
-      allowNUll: false
-    },
-    description: DataType.TEXT
->>>>>>> database setup
   });
 
   return Menu;
