967cbd75170aed25dc25650396467f44842466b7
ststar8623 <ststar8623@gmail.com>
2017-09-04 00:55:11 -0700

    configure passport.js

--- api/middleware/passport.js
+++ api/middleware/passport.js
@@ -22,6 +22,7 @@ passport.deserializeUser((id, done) => {
     });
 });
 
+<<<<<<< 09efc4784254031c692bd99e7d37e5cfbd499ba6
 passport.use(new FacebookStrategy({
   clientID: config.facebook.clientID,
   clientSecret: config.facebook.clientSecret,
@@ -138,3 +139,82 @@ passport.use(new TwitterStategy({
 }));
 
 module.exports = passport;
+=======
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: config.facebook.clientID,
+      clientSecret: config.facebook.clientSecret,
+      callbackURL: config.facebook.callbackURL,
+      profileFields: ['id', 'name', 'email', 'photos']
+    },
+    (accessToken, refreshToken, profile, done) => {
+      let email = profile.emails[0].value;
+      let facebook_id = profile.id;
+
+      User.findOne({
+        where: {
+          $or: [{ facebook_id }, { email }]
+        }
+      })
+        .then(async user => {
+          if (user && !user.dataValues.facebook_id) {
+            await user.update({ facebook_id });
+          } else if (!user) {
+            user = await User.create({
+              email: email,
+              facebook_id: facebook_id,
+              firstName: profile.name.givenName,
+              lastName: profile.name.familyName,
+              facebook_profile_image: profile.photos[0].value
+            });
+          }
+          done(null, user.dataValues);
+        })
+        .catch(err => {
+          done(err, false);
+        });
+    }
+  )
+);
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: config.google.clientID,
+      clientSecret: config.google.clientSecret,
+      callbackURL: config.google.callbackURL
+    },
+    (accessToken, refreshToken, profile, done) => {
+      let email = profile.emails[0].value;
+      let google_id = profile.id;
+
+      User.findOne({
+        where: {
+          $or: [{ google_id }, { email }]
+        }
+      })
+        .then(async user => {
+          if (user && !user.dataValues.google_id) {
+            await user.update({ google_id });
+          } else if (!user) {
+            user = await User.create({
+              email: email,
+              google_id: google_id,
+              firstName: profile.name.givenName,
+              lastName: profile.name.familyName,
+              google_profile_image: profile.photos[0].value
+            });
+          }
+          done(null, user.dataValues);
+        })
+        .catch(err => {
+          console.log('google oauth err: '.red, err);
+          done(err, false);
+        });
+    }
+  )
+);
+
+module.exports = passport;
+>>>>>>> configure passport.js
--- components/layout/layout.js
+++ components/layout/layout.js
@@ -15,7 +15,7 @@ class Layout extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showSidebar: false,
+      showSidebar: false
     };
 
     this.toggleSidebar = this.toggleSidebar.bind(this);
@@ -27,54 +27,67 @@ class Layout extends React.Component {
 
   render() {
     const menus = [
-      {key: 'order', href: '/', name: 'Order'},
-      {key: 'history', href: '/history', name: 'History'},
-      {key: 'setting', href: '/setting', name: 'Setting'},
-      {key: 'about', href: '/about', name: 'About'}
+      { key: 'order', href: '/', name: 'Order' },
+      { key: 'history', href: '/history', name: 'History' },
+      { key: 'setting', href: '/setting', name: 'Setting' },
+      { key: 'about', href: '/about', name: 'About' }
     ];
 
     return (
       <div>
         <Head>
           <title>No-line</title>
-          <meta charSet='utf-8' />
-          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-          <link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css'></link>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <link
+            rel="stylesheet"
+            href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"
+          />
         </Head>
-        <Nav toggleSidebar={this.toggleSidebar} showSidebar={this.state.showSidebar}/>
+        <Nav
+          toggleSidebar={this.toggleSidebar}
+          showSidebar={this.state.showSidebar}
+        />
         <Sidebar.Pushable>
           <Sidebar
             as={Menu}
-            animation='slide along'
+            animation="slide along"
             visible={this.state.showSidebar}
             vertical
             inverted
-            width='thin'
+            width="thin"
           >
-            {menus.map(menu => 
-              <Link href={menu.href} key={menu.key} passHref prefetch> 
+            {menus.map(menu => (
+              <Link href={menu.href} key={menu.key} passHref prefetch>
                 <Menu.Item link>{menu.name}</Menu.Item>
               </Link>
-            )}
+            ))}
             {this.props.session.isLoggedIn ? (
-              <Menu.Item href='signout' link>Sign out</Menu.Item>
+              <Menu.Item href="signout" link>
+                Sign out
+              </Menu.Item>
             ) : (
-              <Link href='/signin' passHref prefetch>
+              <Link href="/signin" passHref prefetch>
                 <Menu.Item link>Sign in</Menu.Item>
               </Link>
             )}
           </Sidebar>
           <Sidebar.Pusher>
-            <Segment 
+            <Segment
               inverted
-              color='grey'
-              style={{height: '100vh', borderRadius: 0}}
+              color="grey"
+              style={{ height: '100vh', borderRadius: 0 }}
             >
               {this.props.children}
             </Segment>
           </Sidebar.Pusher>
         </Sidebar.Pushable>
-        <style jsx global>{nprogressStyle}</style>
+        <style jsx global>
+          {nprogressStyle}
+        </style>
       </div>
     );
   }
