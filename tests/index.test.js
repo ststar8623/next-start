52ff6ddd230c8b175f5fd86aba2fce1ca67283cc
ststar8623 <ststar8623@gmail.com>
2017-09-05 15:32:03 -0700

    Jest Testing / Merging

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,12 +1,12 @@
-import { shallow } from 'enzyme';
-import React from 'react';
-import renderer from 'react-test-renderer';
+const { shallow } = require('enzyme');
+const React = require('react');
+const renderer = require('react-test-renderer');
 
-import Index from '../pages/';
+const Index = '../pages/index.js';
 
 describe('With Enzyme', () => {
   it('Index shows "Application Content"', () => {
-    const index = shallow(<Index />);
+    const index = shallow(<Index />).dive();
 
     expect(index.find('h3').text()).toEqual('Application Content');
   });
